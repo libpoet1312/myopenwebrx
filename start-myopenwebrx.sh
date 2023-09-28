157dae160de47b292d195c09a4043b08550beb13
libpoet1312 <npappas1312@gmail.com>
2023-09-28 20:18:20 +0300

    Fix script

--- start-myopenwebrx.sh
+++ start-myopenwebrx.sh
@@ -3,7 +3,8 @@
 number0=$(lsusb | awk "/RTL2838/ {print $2}")
 number1=$(lsusb | awk "/RTL2838/ {print $4}")
 
-path=$number0/${number1%?}
+number1=${number1%?}
+path=$number0/$number1
 
 echo "Found rtl-sdr on bus $path"
 
